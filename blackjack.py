0d2e42486041dbab6d434fb1bb939b27f9dbdd5e
Phillip <phillipjoel987@gmail.com>
2025-08-10 17:12:50 +0200

    Created hand_total function

--- blackjack.py
+++ blackjack.py
@@ -45,7 +45,7 @@ def bet_ammount(credits):
             print("Please enter a number value between $1 and $20 ")
    
 def create_deck():
-    suits = ["Diamonds", "Spades", "Hearts", "Clubs"]
+    suits = ["♦", "♠", "♥", "♣"]
     ranks = ["2","3","4","5","6","7","8","9","10", "Jack" , "Queen", "King", "Ace" ]
     deck = []
 
@@ -73,74 +73,77 @@ def deal_card(deck):
         #print(f"Your card is {card}")
         return card
         
+# Added hand_total to evaulate a given hand and output point total
+def hand_total(hand):
+    nr_aces = 0
+    point_total = 0
+    for card in hand:
+        point_total += card["value"]
+        if card["rank"] == "Ace":
+            nr_aces += 1
+        if point_total > 21 and nr_aces != 0:
+            point_total -= 10
+            nr_aces -= 1
+    return point_total
+
 
 def blackjack():
+    
     while True:
         play_again = input("Are you ready to play? (y/n)").lower()
-        
+        deck = create_deck()
         if play_again == "y":
-            time.sleep(1.2)
-            #bet_ammount(credits)
-            deck = create_deck()
-
+            
+            ###
             dealer_cards = []
             player_cards = []
-            d_total = 0
-            p_total = 0
-
-            #####
-
-            
-
+                
+            ####
             player_cards.append(deal_card(deck))
+            print(f"Your first card is: \n {player_cards[0]}")
+            dealer_cards.append(deal_card(deck))
+            print(f"Dealers face up card is: \n {dealer_cards[0]}")
             player_cards.append(deal_card(deck))
-
-            #print(f"Your face up card is {player_cards[0]}")
-            #print(f" You were delt: {player_cards[[0]["rank"]]} of {player_cards[[0]["suit"]]} {player_cards[[0]["value"]]} ")
-            
+            time.sleep(4)
+            print(f"Your next card is {player_cards[1]}")
             dealer_cards.append(deal_card(deck))
-            dealer_cards.append(deal_card(deck))
-            print(f"Dealers face up card is {dealer_cards[0]}")
 
-            d_total = sum(card["value"] for card in dealer_cards)
-            p_total = sum(card["value"] for card in player_cards)
-            
-            #for i, card in enumerate(player_cards):
-             #   print(f"Your {i+1} card is {player_cards[i]}")
-            
+            d_total = hand_total(dealer_cards)
+            p_total = hand_total(player_cards)
+            ###
             while True:
-                if p_total >= 21 and d_total >= 17:
-                    print("You win ", p_total , d_total)
-                    
+
+                time.sleep(1.2)
+                #bet_ammount(credits
+                
+                ###Check for blackjack
+                if p_total == 21 and d_total == 21:
+                    print("Both have Blackjack — push (draw).")
+                    break
+                elif p_total == 21:
+                    print("Blackjack! You win!")
+                    break
+                elif d_total == 21:
+                    print(f"Dealer’s cards are {dealer_cards}\nDealer has {d_total} points.\nYou lose!")
+                    break
+                                #if p_total >= 21 and d_total >= 17:
+                #    print("You win \n dealers cards were {dealer_cards}\n Your total was: {p_total}\nDealer Total was: {d_total}")
+                #    break
+                
+                
                 print(f"\nYour cards are {player_cards}")
                 print("Your cards total is:" , p_total)
 
                 hit_stand = input("Would you like to hit or stand : ").lower()
                 
                 if hit_stand == "hit":
+                    #Deal out a card 
                     new_card = deal_card(deck)
+                    #Add card to players hand
                     player_cards.append(new_card)
-
-                    #####
-                    used_ace = False
-                    for cards in player_cards:
-                        if cards["rank"] == "Ace":
-                            used_ace = True
-
-                        
-                        if p_total > 21 and used_ace :
-                            p_total -= 10
-                            print("ran p total is", p_total)
-                    if used_ace:
-                        for cards in player_cards:
-                            if cards["rank"] == "Ace":
-                                cards["value"] = 1
-                                print("ran")
-                    
-                    
-
-                    #####
-                    p_total = sum(card["value"] for card in player_cards)
+                    #Get players total
+                    p_total = hand_total(player_cards)
+            
                     #p_total += new_card["value"]
                     print(f"You pulled {new_card} \n  Your total is {p_total}")
                     time.sleep(1.1)
@@ -156,10 +159,13 @@ def blackjack():
                     while d_total < 17:
                         print(f"Dealers card are {dealer_cards} \nDealers Total is: {d_total}")
 
+                        #Dealer deals for himself
                         new_card = deal_card(deck)
+                        #Dealer adds it to his deck
                         dealer_cards.append(new_card)
+                        #Dealers Total hand value
+                        d_total = hand_total(dealer_cards)
                         
-                        d_total += new_card["value"]
                         print(f"Dealer Pulled a {new_card}\nDealers Total is: {d_total}")
                         time.sleep(1.5)
                         if d_total > 21:
@@ -177,26 +183,15 @@ def blackjack():
                         print(f"You lose! \n dealer had {d_total} to your {p_total}")
                         break
                 
-                elif p_total == 21:
-                    while d_total < 17:
-                        print(f"Dealers card are {dealer_cards} \n")
-
-                        new_card = deal_card(deck)
-                        dealer_cards.append(new_card)
-                        
-                        d_total += new_card["value"]
-                        print(f"Dealer Pulled a {new_card}")
-                        if d_total > 21:
-                            print("Dealer busts you win!")
-                            break
+                
                 else:
                     print("Please enter (hit) or (stand)")
 
 
 
         elif play_again =="n":
-            c_credits = credits + bet
-            print(f'Your bet of ${bet} has been returned')
+           # c_credits = credits + bet
+           # print(f'Your bet of ${bet} has been returned')
             break
         else:
             print("Please only enter (Y/N) ")
@@ -207,7 +202,7 @@ def blackjack():
 
 if __name__ == "__main__":  
     blackjack_greeting()  
-    credits = c_credits()     
-    credits, bet = bet_ammount(credits)
+   # credits = c_credits()     
+   # credits, bet = bet_ammount(credits)
     blackjack()
     
