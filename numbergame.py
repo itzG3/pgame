a7ff85f89f279d9c0279903e3743ffc41a2bf462
Phillip <phillipjoel987@gmail.com>
2025-09-19 12:54:50 +0200

    Changes range from 100 to 500

--- numbergame.py
+++ numbergame.py
@@ -5,24 +5,24 @@ import random
 
 def welcome():
     print("\nWelcome Higher or Lower I hope that you enjoy")
-    print("Your Job is to guess the random number between 1 and 100")
+    print("Your Job is to guess the random number between 1 and 500")
     print("You will be told if the number is higher or lower \n")
 
 
 def higherlower():
-    answer = random.randint(1,100)
-    nr_guesses = 8 
+    answer = random.randint(1,500)
+    nr_guesses = 10 
 
     for i in range(nr_guesses):
 
         while True:
-            guess = input("Please enter your guess from 1-100: ")
+            guess = input("Please enter your guess from 1-500: ")
             
         
             try:
                 uguess = int(guess)
 
-                if not (1 <= uguess <= 100):
+                if not (1 <= uguess <= 500):
                         print("Please enter a number between 1 and 100")
                 else:
                     break
@@ -33,14 +33,14 @@ def higherlower():
         # Game outputs 
 
         if uguess == answer:
-            print(f"\n You win! \n It only took {i} guesses! \n")
+            print(f"\n You win! \n It only took {i+1} guesses! \n")
             break
         elif i == 7:
             print("You lose better luck next time! \n")
         elif uguess > answer:
-            print(f"Your guess was higher, you have {7-i} guesses remaining \nGuess a little lower (; ")
+            print(f"Your guess was higher, you have {9-i} guesses remaining \nGuess a little lower (; ")
         elif uguess < answer:
-            print(f"Your guess was lower, you have {7-i} guesses remaining \nGuess a little higher (; ")
+            print(f"Your guess was lower, you have {9-i} guesses remaining \nGuess a little higher (; ")
         else:
             break
 
